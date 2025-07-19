658412abf9c7ee551ab091d26b6cc3b4ecb1b575
Cindy Zhao <xintongzhao@ucsb.edu>
2025-07-19 11:13:47 -0700

    edit template for game

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -9,13 +9,18 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background');
+        this.add.image(960, 540, 'background').setScale(2);
 
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        this.add.text(960, 350, 'All Around Algae', {
+            fontFamily: 'Arial Black', fontSize: 100, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.add.text(960, 650, 'Click to Start', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8, backgroundColor: '#92f8c8ff',
+            padding: { left: 15, right: 15, top: 10, bottom: 10 },
             align: 'center'
         }).setOrigin(0.5);
 
