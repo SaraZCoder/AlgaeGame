04d94c4beb004e2b1ba69131865e9b9896ce1c79
Cindy Zhao <xintongzhao@ucsb.edu>
2025-07-26 16:42:11 -0700

    refactor animation function logic

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -6,6 +6,7 @@ export class Game extends Scene
     {
         super('Game');
         this.dialogue = null; // define dialogue property
+        this.complete = false; // game complete?
     }
 
     create ()
@@ -147,11 +148,86 @@ export class Game extends Scene
         this.scoreMessage.setText(this.score + '/5');
         this.photoIsVisible = true;
 
+        this._addAnimation('animation-photo', photoName, textPages, () => {
+            // This callback runs when the animation is complete
+            if (this.score === 5) {
+                this.complete = true;
+                const outroText = this.dialogue.Conclusion;
+                const photoIcon = this.add.image(0, 0, 'photo-icon-light').setOrigin(0, 0).setInteractive({ pixelPerfect: true }).setScrollFactor(0);
+                this.scoreMessage.setText('');
+                
+                // Add pulsing glow effect
+                this.tweens.add({
+                    targets: photoIcon,
+                    alpha: 0.5,
+                    duration: 800,
+                    ease: 'Sine.easeInOut',
+                    yoyo: true,
+                    repeat: -1 // Infinite repeat
+                });
+                
+                // Add click handler
+                photoIcon.on('pointerdown', () => {
+                    this.tweens.killTweensOf(photoIcon); // Stop the glow animation
+                    photoIcon.setAlpha(1); // Reset alpha
+                    this.friendAnimation(outroText, 2);
+                });
+            }
+        });
+    }
+    
+    friendAnimation (textPages = ['default text', 'yippee!'], numScene = 1) {
+        this.photoIsVisible = true;
+        
+        if (numScene === 2) {
+            this._addAnimation('animation-friend', '', textPages, () => {
+                // Post-animation logic for scene 2
+                this.cameras.main.fadeOut(1000, 0, 0, 0);
+                this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                    this.scene.start('GameOver');
+                });
+            });
+        } else {
+            // No delay for other scenes
+            this._addAnimation('animation-friend', '', textPages, () => {
+                // Post-animation logic for scene 1
+                if (numScene === 1) {
+                    const gameInstr = 'Click and drag to move around. Click on items to explore.';
+                    const instr = this.add.text(this.cameras.main.width / 2, 100, gameInstr, 
+                        { 
+                            font: '48px Arial', 
+                            color: '#000000', 
+                            backgroundColor: '#ffffff',
+                            padding: { x: 10, y: 5 }
+                        }
+                    ).setOrigin(0.5, 0).setAlpha(0);
+                    
+                    instr.setScrollFactor(0);
+                    
+                    this.tweens.add({
+                        targets: instr,
+                        alpha: 1,
+                        duration: 500,
+                        ease: 'Power2',
+                        onComplete: () => {
+                            this.input.once('pointerdown', () => {
+                                instr.destroy();
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    _addAnimation (image, photoName = '', textPages, onAnimationComplete = null)
+    {
+        // bg is either 'animation-friend' or 'animation-photo'
         // Show photo and caption for input photo name and text
         const bg = this.add.rectangle(this.cameras.main.width / 2, this.cameras.main.height / 2, this.cameras.main.width, this.cameras.main.height, 0xffffff, 0.5).setAlpha(0);
         bg.setScrollFactor(0).setInteractive();
 
-        const photoFrame = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'animation-photo').setAlpha(0);
+        const photoFrame = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, image).setAlpha(0);
         photoFrame.setScrollFactor(0).setInteractive({ pixelPerfect: true });
         
         const text = this.add.text(this.cameras.main.width / 2 - 550, this.cameras.main.height / 2 + 200, '', { font: '40px Arial', fill: '#000000', wordWrap: { width: 1080 }, align: 'left' }).setOrigin(0, 0);
@@ -163,17 +239,15 @@ export class Game extends Scene
         const continueText = this.add.text(photoFrame.x - 150, photoFrame.y + 425, 'Click anywhere to continue', { font: '24px Arial', fill: '#515151ff', align: 'center' }).setOrigin(0);
         continueText.setScrollFactor(0).setAlpha(0);
         
-        const targets = [bg, photoFrame, text, friendText, continueText];
+        const targets = [bg, photoFrame, text, friendText, continueText]; // targets for animation
 
         let itemPhoto;
         if (photoName) {
             itemPhoto = this.add.image(photoFrame.x, photoFrame.y - 224, photoName).setAlpha(0);
             itemPhoto.setScrollFactor(0);
-            itemPhoto.setDisplaySize(1022, 486);
+            itemPhoto.setDisplaySize(1022, 486); // TODO: might need to change if photo already positioned
 
             targets.push(itemPhoto);
-        } else {
-            this.add.text(this.cameras.width / 2, this.cameras.height / 2, 'Photo unavailable', { font: '80px Arial', fill: '#000000' });
         }
 
         // Add animation
@@ -215,124 +289,21 @@ export class Game extends Scene
                                         itemPhoto.destroy();
                                     }
                                     this.photoIsVisible = false;
-                                    // Show friend animation once we reach 5/5
-                                    if (this.score === 5) {
-                                        const outroText = this.dialogue.Conclusion;
-                                        this.friendAnimation(outroText, 2); // TODO: Add delay
-                                        this.add.image(0, 0, 'photo-icon-light').setOrigin(0, 0).setInteractive({ pixelPerfect: true }).setScrollFactor(0);
-                                        this.scoreMessage.setText('');
+                                    
+                                    // Call the completion callback here
+                                    if (onAnimationComplete) {
+                                        onAnimationComplete();
                                     }
                                 }
                             });
                         }
                     }
                 };
-
                 this.input.on('pointerdown', clickHandler);
             }
         });
     }
-
-    friendAnimation (textPages = ['default text', 'yippee!'], numScene) {
-        this.photoIsVisible = true;
-
-        // Show friend animation
-        const bg = this.add.rectangle(this.cameras.main.width / 2, this.cameras.main.height / 2, this.cameras.main.width, this.cameras.main.height, 0xffffff, 0.5).setAlpha(0);
-        bg.setScrollFactor(0).setInteractive();
-
-        const photoFrame = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'animation-friend').setAlpha(0).setDisplaySize(1920, 1080);
-        photoFrame.setScrollFactor(0).setInteractive({ pixelPerfect: true });
-        
-        const text = this.add.text(this.cameras.main.width / 2 - 550, this.cameras.main.height / 2 + 200, '', { font: '40px Arial', fill: '#000000', wordWrap: { width: 1080 }, align: 'left' }).setOrigin(0, 0);
-        text.setScrollFactor(0).setAlpha(0);
-
-        const friendText = this.add.text(photoFrame.x - 487, photoFrame.y + 97, 'Friend', { font: '40px Arial', fill: '#000000' });
-        friendText.setScrollFactor(0).setAlpha(0);
-
-        const continueText = this.add.text(photoFrame.x - 150, photoFrame.y + 425, 'Click anywhere to continue', { font: '24px Arial', fill: '#515151ff', align: 'center' }).setOrigin(0);
-        continueText.setScrollFactor(0).setAlpha(0);
-        
-        const targets = [bg, photoFrame, text, friendText, continueText];
-
-        this.tweens.add({
-            targets: targets,
-            alpha: 1,
-            duration: 500,
-            onComplete: () => {
-                let currentPage = 0;
-                let isTyping = true;
-
-                this.typewriterEffect(text, textPages[currentPage], () => {
-                    isTyping = false;
-                });
-
-                const clickHandler = () => {
-                    if (isTyping) {
-                        this.typewriterEffect(text, textPages[currentPage], () => {
-                            isTyping = false;
-                        }, true);
-                    } else {
-                        currentPage++;
-                        if (currentPage < textPages.length) {
-                            isTyping = true;
-                            this.typewriterEffect(text, textPages[currentPage], () => {
-                                isTyping = false;
-                            });
-                        } else {
-                            this.input.off('pointerdown', clickHandler);
-                            this.tweens.add({
-                                targets: targets,
-                                alpha: 0,
-                                duration: 500,
-                                onComplete: () => {
-                                    bg.destroy();
-                                    photoFrame.destroy();
-                                    text.destroy();
-                                    this.photoIsVisible = false;
-                                    if (numScene === 1) {
-                                        // Show instructions for the game
-                                        const gameInstr = 'Click and drag to move around. Click on items to explore.';
-                                        const instr = this.add.text(this.cameras.main.width / 2, 100, gameInstr, 
-                                            { 
-                                                font: '48px Arial', 
-                                                color: '#000000', 
-                                                backgroundColor: '#ffffff',
-                                                padding: { x: 10, y: 5 }
-                                            }
-                                        ).setOrigin(0.5, 0).setAlpha(0);
-
-                                        instr.setScrollFactor(0); // Keep it fixed on the screen
-
-                                        this.tweens.add({
-                                            targets: instr,
-                                            alpha: 1,
-                                            duration: 500,
-                                            ease: 'Power2',
-                                            onComplete: () => {
-                                                this.input.once('pointerdown', () => {
-                                                    instr.destroy();
-                                                });
-                                            }
-                                        });
-                                    }
-                                    if (numScene === 2) {
-                                        // Move on to the end scene
-                                        this.cameras.main.fadeOut(1000, 0, 0, 0);
-                                        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-                                            this.scene.start('GameOver');
-                                        });
-                                    }
-                                }
-                            });
-                        }
-                    }
-                };
-
-                this.input.on('pointerdown', clickHandler);
-            }
-        });
-    }
-
+    
     typewriterEffect(textObject, text, onComplete, skip = false) {
         if (textObject.typewriterTimer) {
             textObject.typewriterTimer.remove();
@@ -373,6 +344,9 @@ export class Game extends Scene
     }
 
     showTemporaryText(text) {
+        if (this.complete === true) {
+            text = 'You found all of the items! Please click your camera roll to finish.'
+        }
         const textObject = this.add.text(
             this.cameras.main.centerX,
             100, // At the top of the screen
@@ -390,7 +364,7 @@ export class Game extends Scene
         this.tweens.add({
             targets: textObject,
             alpha: 0,
-            duration: 1000 +(50 * text.length),
+            duration: 1000 +(100 * text.length),
             ease: 'Power2',
             delay: 1000,
             onComplete: () => {
